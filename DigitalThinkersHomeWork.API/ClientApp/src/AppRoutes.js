b8ba3efd9c71cad7ef66e53d418de657f8abfecd
Péter Koppány <koppany.peter@autsoft.hu>
2023-03-06 18:56:23 +0100

    Remove unnecessary frontend code and packages

--- DigitalThinkersHomeWork.API/ClientApp/src/AppRoutes.js
+++ DigitalThinkersHomeWork.API/ClientApp/src/AppRoutes.js
@@ -7,14 +7,6 @@ const AppRoutes = [
     index: true,
     element: <Home />
   },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
 ];
 
 export default AppRoutes;
